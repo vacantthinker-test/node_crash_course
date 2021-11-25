9b12fdec4627ace9b34b5c0f8226ddc46306f416
vacantthinker <littlehappyforsmile@outlook.com>
2021-11-25 14:53:55 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const server = http.createServer((req, res) => {
+	console.log('req.url:',req.url)
+	// if (req.url === '/') {
+	// 	fs.readFile(
+	// 		path.join(__dirname, 'public', 'index.html'),
+	// 		(err, content) => {
+	// 			if (err) throw err
+	// 			res.writeHead(200, {'Content-Type': 'text/html'})
+	// 			// res.end(`<h1>home</h1>`)
+	// 			res.end(content)
+	// 		})
+	// }
+	// if (req.url === '/about') {
+	// 	fs.readFile(
+	// 		path.join(__dirname, 'public', 'about.html'),
+	// 		(err, content) => {
+	// 			if (err) throw err
+	// 			res.writeHead(200, {'Content-Type': 'text/html'})
+	// 			// res.end(`<h1>home</h1>`)
+	// 			res.end(content)
+	// 		})
+	// }
+	//
+	// if (req.url === '/api/users') {
+	// 	const users = [
+	// 		{name: 'bob smith', age: 40},
+	// 		{name: 'john doe', age: 30},
+	// 	]
+	// 	res.writeHead(200, {'Content-Type': 'application/json'})
+	// 	res.end(JSON.stringify(users))
+	// }
+	
+	// build file path
+	let filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url)
+	// extension of file
+	let extname = path.extname(filePath)
+	
+	// initial content type
+	let contentType = 'text/html'
+	// check ext and set content type
+	switch (extname) {
+		case '.js':
+			contentType = 'text/javascript'
+			break;
+		case '.css':
+			contentType = 'text/css'
+			break;
+		case '.json':
+			contentType = 'application/json'
+			break;
+		case '.png':
+			contentType = 'image/png'
+			break;
+		case '.jpg':
+			contentType = 'image/jpg'
+			break;
+	}
+	
+	// read file
+	fs.readFile(filePath, (err, content)=>{
+		if (err){
+			if (err.code === 'ENOENT'){
+				// page not found
+				fs.readFile(path.join(__dirname, 'public', '404.html'), (err, content)=>{
+					res.writeHead(404, {'Content-Type': 'text/html'})
+					res.end(content, 'utf8')
+				})
+			}else {
+				// some server error
+				res.writeHead(500)
+				res.end(`server err: ${err.code}`)
+			}
+		}else {
+			// success
+			res.writeHead(200, {'Content-type': contentType})
+			res.end(content, 'utf8')
+			
+		}
+	})
+})
+const PORT = Number(process.env.PORT) || 5000
+server.listen(PORT, 'localhost',null,() => {
+	console.log(`server is running on ${PORT}`)
+})
